723604856c016f27d07c177c529e27a0caaf98f0
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-03-25 12:42:52 +0600

    theme setup complate

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import { AiFillHome  } from "react-icons/ai";
 import { AiFillInfoCircle } from "react-icons/ai";
 import Link from 'next/link';
+import DarkMoodSwitch from './DarkMoodSwitch';
 
 const Header = () => {
   return (
@@ -11,10 +12,14 @@ const Header = () => {
        <MenuItem title="Home" icons={<AiFillHome/>} address='/'/>
         <MenuItem title="about" icons={<AiFillInfoCircle/>} address='/about'/>
        </div>
-       <Link href={"/"} className=' flex gap-2 items-center transform-500'>
+        <div className=" flex gap-4 items-center">
+        <DarkMoodSwitch/>
+          <Link href={"/"} className=' flex gap-2 items-center transform-500'>
         <span className=' text-2xl uppercase py-1 px-2 bg-amber-500 rounded-lg font-bold'>imdb</span>
         <span className=' text-xl hidden sm:inline'>clone</span>
        </Link>
+        </div>
+       
     </div>
   )
 }

6e61f667216f2898605a1a7f2b67b0f5025a9f98
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-03-25 10:34:53 +0600

    header section complate

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import MenuItem from './MenuItem'
 import { AiFillHome  } from "react-icons/ai";
+import { AiFillInfoCircle } from "react-icons/ai";
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div>
-        <MenuItem title="Home" icons={<AiFillHome/>} address='/'/>
-        <MenuItem title="about" icons={<AiFillHome/>} address='/about'/>
+    <div className=' flex items-center justify-between py-3 max-w-6xl mx-auto '>
+       <div className='flex gap-4'>
+       <MenuItem title="Home" icons={<AiFillHome/>} address='/'/>
+        <MenuItem title="about" icons={<AiFillInfoCircle/>} address='/about'/>
+       </div>
+       <Link href={"/"} className=' flex gap-2 items-center transform-500'>
+        <span className=' text-2xl uppercase py-1 px-2 bg-amber-500 rounded-lg font-bold'>imdb</span>
+        <span className=' text-xl hidden sm:inline'>clone</span>
+       </Link>
     </div>
   )
 }
--- src/app/Components/MenuItem.jsx
+++ src/app/Components/MenuItem.jsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 
 const MenuItem = ({title, address, icons}) => {
   return (
-   <Link href={address || '#'}>
-   <icons>{icons}</icons>
-   <p>{title}</p>
+   <Link href={address || '#'} className='hover:text-amber-500'>
+   <icons className='sm:hidden text-2xl'>{icons}</icons>
+   <p className='uppercase hidden sm:inline text-sm'>{title}</p>
    </Link>
   )
 }

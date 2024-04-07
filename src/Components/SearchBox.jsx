78b8807b92e429509656401d404d7e994a784694
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-04-07 15:57:10 +0600

    Search component start

---
+++ src/Components/SearchBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SearchBox = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="text " placeholder='Search here.....' className='p-4 w-full outline-none placeholder-gray-500 ' />
+            <button>Search</button>
+        </form>
+    </div>
+  )
+}
+
+export default SearchBox
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/Components/Header";
 import Provider from "./Providers";
 import Navbar from "@/Components/Navbar";
+import SearchBox from "@/Components/SearchBox";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Provider>
         <Header/>
         <Navbar/>
+        <SearchBox/>
         {children}
         </Provider>
         

efc896078f39ad0a3048a45c70150be03ab7967d
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-04-09 00:02:51 +0600

    search page

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+'use client'
+import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 const SearchBox = () => {
+  const [search, setSearch] = useState('')
+  const router = useRouter()
+  const submitHandle = (e)=>{
+    
+       e.preventDefault()
+  }
+  
   return (
     <div>
-        <form action="">
-            <input type="text " placeholder='Search here.....' className='p-4 w-full outline-none placeholder-gray-500 ' />
-            <button>Search</button>
+        <form action="" className="flex justify-between px-5" onSubmit={submitHandle} >
+            <input type="text " placeholder='Search here.....' className='p-4 w-full outline-none placeholder-gray-500 h-14 rounded-md ' value={search} onChange={(e)=>setSearch(e.target.value)}/>
+            <button disable={search=== ''} className='text-amber-500 disabled:text-gray-400' >Search</button>
         </form>
     </div>
   )

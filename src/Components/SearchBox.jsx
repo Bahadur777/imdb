fac2cc81de7dba68e56d7dfeb36fc2a5ee31b18f
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-04-09 12:42:17 +0600

    button modifi

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -14,7 +14,7 @@ const SearchBox = () => {
     <div>
         <form action="" className="flex justify-between px-5" onSubmit={submitHandle} >
             <input type="text " placeholder='Search here.....' className='p-4 w-full outline-none placeholder-gray-500 h-14 rounded-md ' value={search} onChange={(e)=>setSearch(e.target.value)}/>
-            <button disable={search=== ''} className='text-amber-500 disabled:text-gray-400' >Search</button>
+            <button disable={search=== ''} className='text-amber-500 bg-white px-5 rounded-md disabled:text-gray-400' >Search</button>
         </form>
     </div>
   )
